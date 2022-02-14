e034bcd05d510296837655769137186bfb285cab
Jakub Wierzbicki <j.wierzbicki2001@gmail.com>
2022-02-14 02:10:23 +0100

    Search Algorithms added to the repository

---
+++ Search_Algorithms/Binary_Search_RECURSION/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_RECURSION
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = { 1, 2, 2, 2, 3, 4, 6, 8, 11, 16 };
+            Console.WriteLine(Binary_Search(data, 0, 9, 3));
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// algorithm gives you the indxes, where is the value, which you are looking for
+        /// </summary>
+        /// <param name="array">the aray in which we are looking for the value</param>
+        /// <param name="f">it's the variable, which means the first index int the interval</param>
+        /// <param name="l">it's the variable, which means the last index int the interval</param>
+        /// <param name="s">it's the variable, which means the searched value </param>
+        //the binary search algorithm assumes that the array is sorted increasingly
+
+        public static int Binary_Search(int[] array, int f, int l, int s)
+        {
+            if (f >= l && array[f] == s) return f;
+            else if (f >= l) return -1;
+
+            int m = (f + l) / 2;
+            if (array[m] >= s) return Binary_Search(array, f, m, s);
+            else return Binary_Search(array, m + 1, l, s);
+        }
+    }
+}
---
+++ Search_Algorithms/Second_Raw_Leader_Search_ITERATION/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second_Raw_Leader_Search_ITERATION
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = { 1, 2, 2, 2, 1, 1, 3, 4, 5, 1, 2, 1, 2, 5, 6, 7, 8, 9, 1, 1, 2, 3, 1, 1, 1, 2, 2, 2, 2 };
+            Leader2_Search(data);
+            Console.ReadKey();
+        }
+        //the algorithm returns to us two elements, that are more frequent than 33% in array
+        public static void Leader2_Search(int[] array)
+        {
+            if (array.Length < 2) throw new Exception("the array is too short");
+            int leader1 = array[0];
+            int leader2 = array[1];
+            int counter1 = 1, counter2 = 1;
+            for (int i = 2; i < array.Length; i++)
+            {
+                if (counter1 == 0)
+                {
+                    leader1 = array[i];
+                    counter1 = 1;
+                }
+                else if (counter2 == 0)
+                {
+                    leader2 = array[i];
+                    counter2 = 1;
+                }
+                else if (array[i] == leader1) counter1++;
+                else if (array[i] == leader2) counter2++;
+                else { counter1--; counter2--; }
+            }
+            Console.WriteLine($"leader 1: {leader1}   leader2: {leader2}");
+        }
+    }
+}
